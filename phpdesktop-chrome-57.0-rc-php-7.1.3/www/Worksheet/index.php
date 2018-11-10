9593739f15b5942524df733386d68c65f2dcd7db
Frank Sin <franksin101@gmail.com>
2018-11-10 11:36:15 +0800

    AJAX Test  Aagain
    Signed-off-by: Frank Sin <franksin101@gmail.com>

--- phpdesktop-chrome-57.0-rc-php-7.1.3/www/Worksheet/index.php
+++ phpdesktop-chrome-57.0-rc-php-7.1.3/www/Worksheet/index.php
@@ -1,1 +1,31 @@
-echo("Print Hello");
+<!DOCTYPE html>
+<html>
+<head>
+<script>
+function showHint(str) {
+    if (str.length == 0) { 
+        document.getElementById("txtHint").innerHTML = "";
+        return;
+    } else {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById("txtHint").innerHTML = this.responseText;
+            }
+        }
+        xmlhttp.open("GET", "gethint.php?q="+str, true);
+        xmlhttp.send();
+    }
+}
+</script>
+</head>
+<body>
+
+<p><b>Start typing a name in the input field below:</b></p>
+<form> 
+First name: <input id="firstName" type="text" onkeyup="showHint(this.value); document.getElementById('lastName').value=this.value;">
+Last name: <input id="lastName" type="text" onchange="showHint(this.value);">
+</form>
+<p>Suggestions: <span id="txtHint"></span></p>
+</body>
+</html>
